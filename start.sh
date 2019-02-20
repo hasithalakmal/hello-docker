0d467532a199da5939e45af0bf313b869ff09175
hasithag <hasitha.gamage@sysco.com>
2019-02-20 08:34:44 +0530

    start sh

---
+++ start.sh
@@ -0,0 +1,19 @@
+#!/bin/sh
+dockerImageTag=$1
+
+mvn clean package -DskipTests
+
+sudo docker build -t hasithagamage/hello-docker:$dockerImageTag .
+
+existingContainer=$(sudo docker ps | grep hasithagamage/hello-docker | awk '{print $1}')
+
+if [ -z "$existingContainer" ]
+then
+	echo "No containers found"
+
+else
+	sudo docker stop $existingContainer
+	sudo docker rm $existingContainer
+fi
+
+sudo docker run  -itd -p 8101:8080  hasithagamage/hello-docker:$dockerImageTag
